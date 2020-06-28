46e2a22ea8853dea660398b21e95e5556e60e221
MinhPhuc1998 <44628658+MinhPhuc1998@users.noreply.github.com>
2020-06-28 21:23:54 +0700

    change

---
+++ public_html/email.php
@@ -0,0 +1,94 @@
+<?php
+// $email and $message are the data that is being
+// posted to this page from our html contact form
+if (isset($_POST['sub'])) {
+$email = $_POST['email'];
+$message =$_POST['message'];
+//$email = $_REQUEST[’email’] ;
+//$message = $_REQUEST[‘message’] ;
+
+// When we unzipped PHPMailer, it unzipped to
+// public_html/PHPMailer_5.2.0
+require('PHPMailer-5.2-stable/class.phpmailer.php');
+
+$mail = new PHPMailer();
+
+// set mailer to use SMTP
+$mail->IsSMTP();
+
+// As this email.php script lives on the same server as our email server
+// we are setting the HOST to localhost
+$mail->Host = "localhost";  // specify main and backup server
+
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+
+// When sending email using PHPMailer, you need to send from a valid email address
+// In this case, we setup a test email account with the following credentials:
+// email: contact@domain.com
+// pass: password
+$mail->Username = "minhphuc130298@gmail.com";  // SMTP username
+$mail->Password = "MP98*sheepbee"; // SMTP password
+
+// $email is the user’s email address the specified
+// on our contact us page. We set this variable at
+// the top of this page with:
+// $email = $_REQUEST[’email’] ;
+$mail->From = $email;
+
+// below we want to set the email address we will be sending our email to.
+$mail->AddAddress("mpbee1302@gmail.com", "Minpukka Coder");
+
+// set word wrap to 50 characters
+$mail->WordWrap = 50;
+// set email format to HTML
+$mail->IsHTML(true);
+
+$mail->Subject = "You have received feedback from your website!";
+
+// $message is the user’s message they typed in
+// on our contact us page. We set this variable at
+// the top of this page with:
+// $message = $_REQUEST[‘message’] ;
+$mail->Body    = $message;
+$mail->AltBody = $message;
+
+if(!$mail->Send())
+{
+echo "Message could not be sent. <p>";
+echo "Mailer Error: " . $mail->ErrorInfo;
+exit;
+}
+echo "Message has been sent";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<!-- Nhúng file CSS -->
+	<link rel="stylesheet" href="style.css" />
+	<title>send email</title>
+</head>
+<body>
+	<form action="email.php" method="POST">
+	<input type="email" placeholder="Email" name="email" id="email" />
+	<textarea name="message" id="message" rows="15" cols="40"></textarea>
+	<input class="submit-form" type='submit' name="sub" value='Submit' />
+	</form>
+</body>
+</html>
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="POST" action="email.php">
+	<input name="email" id="email" type="text" />
+	Message:
+	<textarea name="message" id="message" rows="15" cols="40"></textarea>
+	<input type="submit" value="Submit" name="sub" />
+	</form>
+</body>
+</html>
+ -->
